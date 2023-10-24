5f37df5130d3442833557ed4fe45d84cb3f9ef4c
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-10-23 21:56:26 -0300

    UserContainer component

---
+++ src/components/UserContainer/index.tsx
@@ -0,0 +1,7 @@
+export function UserContainer({ children }: { children?: React.ReactNode }) {
+  return (
+    <section className="w-72 md:w-96 bg-[#fffffff2] rounded-lg border-b-8 border-sky-400 mt-4">
+      {children}
+    </section>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,17 @@
 import { LuSearch } from 'react-icons/lu'
+import { UserContainer } from '../components/UserContainer'
 
 export function Home() {
   return (
     <div className="">
-      <header className="w-96 h-32 rounded-lg flex flex-col bg-[#ffffff33]">
+      <header className="w-96 h-32 rounded-lg flex flex-col gap-3 justify-center bg-[#ffffff33] shadow">
         <h1 className="title">Github Finder</h1>
 
-        {/* input */}
-        <section className="py-2 px-4 flex items-center justify-center">
+        {/* input section */}
+        <section className="px-4 flex items-center justify-center">
           <input
             type="text"
-            className="w-48 rounded-full px-4 py-1 transition-all outline-none focus:w-52"
+            className="w-60 rounded-full px-4 py-1 transition-all outline-none focus:w-64"
           />
 
           <button className="w-8 h-8 rounded-full bg-sky-400 ml-2 flex items-center justify-center text-white hover:scale-110 transition-all">
@@ -18,6 +19,8 @@ export function Home() {
           </button>
         </section>
       </header>
+
+      <UserContainer></UserContainer>
     </div>
   )
 }
