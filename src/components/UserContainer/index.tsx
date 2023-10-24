fb9a763711276bc82babd3c1b069ce5abbda7162
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-10-24 10:21:23 -0300

    css fix

--- src/components/UserContainer/index.tsx
+++ src/components/UserContainer/index.tsx
@@ -1,6 +1,10 @@
 export function UserContainer({ children }: { children?: React.ReactNode }) {
   return (
-    <section className="w-72 md:w-96 bg-[#fffffff2] rounded-lg border-b-8 border-sky-400 mt-4">
+    <section
+      className={`w-72 md:w-96 bg-[#fffffff2] rounded-b-lg  border-b-8 border-sky-400 mt-4 ${
+        children ? 'rounded' : ''
+      }`}
+    >
       {children}
     </section>
   )
